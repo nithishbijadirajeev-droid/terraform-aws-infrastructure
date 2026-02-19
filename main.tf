66154c62f738e7a1d97e6b97a36a514f5b83391f
Nithish Bijadi Rajeev <nithishbijadirajeev@Mac.home>
2026-02-19 18:11:44 -0500

    Fix Terraform formatting

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = var.instance_type
 
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
